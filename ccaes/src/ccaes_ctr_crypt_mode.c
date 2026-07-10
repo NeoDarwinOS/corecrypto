ba4bd25e6db9b9eecdd32435c8cbb69c5388c6a9
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-07-10 17:21:36 +1000

    ccaes: Add CTR mode constructor.
    Signed-off-by: Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>

---
+++ ccaes/src/ccaes_ctr_crypt_mode.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2026, Samuel Zormeister.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include <corecrypto/ccaes.h>
+#include <corecrypto/ccmode_internal.h>
+
+CCMODE_CTR_FACTORY(aes);
