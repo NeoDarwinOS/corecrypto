0367baf9b0199c58f446aa9d7ad3d892247ca6d4
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-08-24 16:07:23 +1000

    cc_absolute_time: Implement for Win32, Darwin and XNU.

--- cc/include/corecrypto/cc_absolute_time.h
+++ cc/include/corecrypto/cc_absolute_time.h
@@ -6,10 +6,42 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-
 #ifndef __CORECRYPTO_CC_ABSOLUTE_TIME_H__
 #define __CORECRYPTO_CC_ABSOLUTE_TIME_H__
 
 #include <corecrypto/cc_config.h>
+#include <stdint.h>
+
+#if CC_PLATFORM_XNU
+    #include <mach/mach_time.h>
+    #include <kern/clock.h>
+
+    CC_INLINE uint64_t cc_absolute_time(void) {
+        return mach_absolute_time();
+    }
+#elif CC_PLATFORM_DARWIN
+    #include <mach/mach_time.h>
+    
+    CC_INLINE uint64_t cc_absolute_time(void) {
+        return mach_absolute_time();
+    }
+#elif CC_PLATFORM_LINUX
+
+//
+// big fat todo here for later.
+//
+
+#elif CC_PLATFORM_WINDOWS
+    #include <windows.h>
+    #include <profileapi.h>
+
+    CC_INLINE uint64_t cc_absolute_time(void) {
+        LARGE_INTEGER count;
+        QueryPerformanceCounter(&count);
+        return count.QuadPart;
+    }
+#else
+    #error unsupported platform
+#endif
 
 #endif /* __CORECRYPTO_CC_ABSOLUTE_TIME_H__ */
