9aeec7ba880b7ec3051e6e7fc1498aa23a79a68e
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-08-30 09:54:42 +1000

    cc: misc

--- ccdes/src/ltc_des_common.h
+++ ccdes/src/ltc_des_common.h
@@ -33,17 +33,23 @@ struct ltc_des3_key {
     uint32_t ek[3][32], dk[3][32];
 };
 
+CC_PRIVATE
 void deskey(const unsigned char *key, short edf, uint32_t *keyout);
+CC_PRIVATE
 void desfunc(uint32_t *block, const uint32_t *keys);
 
+CC_PRIVATE
 int ltc_des3_setup(const unsigned char *key, size_t keylen, int num_rounds, ccecb_ctx *skey);
+CC_PRIVATE
 int ltc_des_setup(const unsigned char *key, size_t keylen, int num_rounds, ccecb_ctx *skey);
 
+CC_PRIVATE
 cc_error_t ccdes_ltc_setup(const struct ccmode_ecb *ecb,
                            ccecb_ctx *ctx,
                            size_t key_size,
                            const void *key);
 
+CC_PRIVATE
 cc_error_t ccdes3_ltc_setup(const struct ccmode_ecb *ecb,
                             ccecb_ctx *ctx,
                             size_t key_size,
