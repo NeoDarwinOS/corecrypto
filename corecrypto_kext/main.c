90f11225286c0bd89ae5dd7ae7656cf3e1a3b29c
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-09-21 20:19:14 +1000

    corecrypto_kext: Register AES-XTS with kernel.

--- corecrypto_kext/main.c
+++ corecrypto_kext/main.c
@@ -119,12 +119,14 @@ void cc_kernel_populate_registration(void)
     corecrypto_registration_if.cchmac_final_fn = (cchmac_final_fn_t)&cchmac_final;
     corecrypto_registration_if.cchmac_fn = (cchmac_fn_t)&cchmac;
 
-    /* MISSING COMPONENTS: ccaes in xts and gcm */
+    /* MISSING COMPONENTS: ccaes in gcm */
     corecrypto_registration_if.ccaes_ecb_encrypt = ccaes_ecb_encrypt_mode();
     corecrypto_registration_if.ccaes_ecb_decrypt = ccaes_ecb_decrypt_mode();
     corecrypto_registration_if.ccaes_cbc_encrypt = ccaes_cbc_encrypt_mode();
     corecrypto_registration_if.ccaes_cbc_decrypt = ccaes_cbc_encrypt_mode();
     corecrypto_registration_if.ccaes_ctr_crypt = ccaes_ctr_crypt_mode();
+    corecrypto_registration_if.ccaes_xts_encrypt = ccaes_xts_encrypt_mode();
+    corecrypto_registration_if.ccaes_xts_decrypt = ccaes_xts_decrypt_mode();
 
     /* MISSING COMPONENTS: ccgcm_[init_with_iv, inc_iv ]  */
 
